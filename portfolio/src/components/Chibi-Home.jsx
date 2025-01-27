8992da41ea791d39edfd2e3f6a9a36711663c82b
Neary <kibishiigaming@gmail.com>
2025-01-27 16:37:09 +0100

    Inscrustation des Chibis.

--- portfolio/src/components/Chibi-Home.jsx
+++ portfolio/src/components/Chibi-Home.jsx
@@ -1,21 +1,9 @@
 import React from 'react';
-import '../styles/chibi.scss';
-import chibiKibi from '../assets/images/chibi-kibi.png';
-import chibiKibiHearts from '../assets/images/chibi-kibi-hearts.png';
-import chibiKibiHey from '../assets/images/chibi-kibi-hey.png';
 
-function ChibisKibi() {
+function ChibisKibi({ image }) {
     return (
-        <div className="chibis-container">
-            <div className="chibi-kibi">
-                <img src={chibiKibi} alt="Kibi" className="chibi-kibi-image" />
-            </div>
-            <div className="chibi-kibi-hearts">
-                <img src={chibiKibiHearts} alt="Kibi Hearts" className="chibi-kibi-hearts-image" />
-            </div>
-            <div className="chibi-kibi-hey">
-                <img src={chibiKibiHey} alt="Kibi Hey" className="chibi-kibi-hey-image" />
-            </div>
+        <div className="chibi-kibi">
+            <img src={image} alt="Kibi" />
         </div>
     );
 }
--- portfolio/src/pages/Home.jsx
+++ portfolio/src/pages/Home.jsx
@@ -1,16 +1,36 @@
 import React from 'react';
 import Banner from "../components/Banner-Home.jsx";
-import Inserts from "../components/Inserts-Home.jsx"
+import Inserts from "../components/Inserts-Home.jsx";
+import ChibisKibi from '../components/Chibi-Home.jsx';
+import chibiKibi from '../assets/images/chibi-kibi.png';
+import chibiKibiHearts from '../assets/images/chibi-kibi-hearts.png';
+import chibiKibiHey from '../assets/images/chibi-kibi-hey.png';
 
 function Home() {
     return (
-        <div>
-            <Banner title="Neary Casolari" />
-            <Inserts title="À propos"/>
-            <Inserts title="Mes Projets"/>
-            <Inserts title="Mes Compétences"/>
-        </div>
+        <>
+            <div>
+                <Banner title="Neary Casolari" />
+            </div>
+            <div className="inserts-container">
+                <div className="insert-item">
+                    <Inserts title="À propos" />
+                    <ChibisKibi image={chibiKibi} /> {/* Passer l'image chibiKibi */}
+                </div>
+                <div className="insert-item">
+                    <Inserts title="Mes Projets" />
+                    <ChibisKibi image={chibiKibiHearts} /> {/* Passer l'image chibiKibiHearts */}
+                </div>
+                <div className="insert-item">
+                    <Inserts title="Mes Compétences" />
+                    <ChibisKibi image={chibiKibiHey} /> {/* Passer l'image chibiKibiHey */}
+                </div>
+            </div>
+        </>
     );
 }
 
 export default Home;
+
+
+
