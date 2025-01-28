b4f7043222d4d0f094fc157003f0ebd3147227c8
Neary <kibishiigaming@gmail.com>
2025-01-28 22:17:57 +0100

    Page 404 finit.

--- portfolio/src/components/404.jsx
+++ portfolio/src/components/404.jsx
@@ -5,7 +5,7 @@ const Error404 = () => {
     return (
     <div className="not-found-container">
         <h1 className="not-found-title">404</h1>
-        <p className="not-found-text">Oups! La page que vous cherchez n'existe pas ^_^</p>
+        <p className="not-found-text">Oupsy! La page que vous cherchez n'existe pas ^_^</p>
         <a href="/" className="not-found-link">
         Retourner à la page d'accueil
         </a>
--- portfolio/src/pages/Error404.jsx
+++ portfolio/src/pages/Error404.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import "../styles/404.scss";
-import cherryBlossomIcon from "../assets/images/cherry-blossom.png"; // Chemin vers votre icône
+import cherryBlossomIcon from "../assets/images/cherry-blossom.png";
 
 const Error404 = () => {
     return (
         <div className="not-found-container">
             <h1 className="not-found-title">404</h1>
-            <p className="not-found-text">Oups! La page que vous cherchez n'existe pas.</p>
+            <p className="not-found-text">Oupsy! La page que vous cherchez n'existe pas ^_^</p>
             <a href="/" className="not-found-link">
                 <span>Retourner à la page d'accueil</span>
                 <img 
