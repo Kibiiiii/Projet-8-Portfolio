679eaeb0769bf7f2afba84a2af8e47b245b9b4b6
Neary <kibishiigaming@gmail.com>
2025-01-28 15:12:22 +0100

    Implementation d'une video en background.

--- portfolio/src/pages/Home.jsx
+++ portfolio/src/pages/Home.jsx
@@ -2,20 +2,34 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Banner from "../components/Banner-Home.jsx";
 import Inserts from "../components/Inserts-Home.jsx";
-import ChibisKibi from '../components/Chibi-Home.jsx';
-import chibiKibi from '../assets/images/chibi-kibi.png';
-import chibiKibiHearts from '../assets/images/chibi-kibi-hearts.png';
-import chibiKibiHey from '../assets/images/chibi-kibi-hey.png';
-import FooterLink from '../components/Footer.jsx';
-import footerLinkInstagram from '../assets/images/instagram.png';
-import footerLinkLinkedin from '../assets/images/linkedin.png';
-import footerLinkGithub from '../assets/images/github.png';
-import footerLinkCv from '../assets/images/cv.png';
-import footerLinkContact from '../assets/images/contact.png';
+import ChibisKibi from "../components/Chibi-Home.jsx";
+import chibiKibi from "../assets/images/chibi-kibi.png";
+import chibiKibiHearts from "../assets/images/chibi-kibi-hearts.png";
+import chibiKibiHey from "../assets/images/chibi-kibi-hey.png";
+import FooterLink from "../components/Footer.jsx";
+import footerLinkInstagram from "../assets/images/instagram.png";
+import footerLinkLinkedin from "../assets/images/linkedin.png";
+import footerLinkGithub from "../assets/images/github.png";
+import footerLinkCv from "../assets/images/cv.png";
+import footerLinkContact from "../assets/images/contact.png";
 
 function Home() {
     return (
         <>
+            {/* Vidéo en arrière-plan */}
+            <div className="video-background">
+                <video
+                    autoPlay
+                    loop
+                    muted
+                    className="background-video"
+                >
+                    <source src="/videos/background-video.mp4" type="video/mp4" />
+                    Votre navigateur ne supporte pas les vidéos.
+                </video>
+            </div>
+
+            {/* Contenu principal */}
             <div>
                 <Banner title="Neary Casolari" />
             </div>
@@ -41,33 +55,27 @@ function Home() {
             </div>
             <div className="footer-links">
                 <div className="link-item">
-                    <FooterLink 
-                        image={footerLinkCv} 
-                        link="/cv.pdf"
+                    <FooterLink image={footerLinkCv} link="/cv.pdf" />
+                </div>
+                <div className="link-item">
+                    <FooterLink image={footerLinkContact} link="#" />
+                </div>
+                <div className="link-item">
+                    <FooterLink
+                        image={footerLinkInstagram}
+                        link="https://www.instagram.com/kibishiii_/?hl=fr"
                     />
                 </div>
                 <div className="link-item">
-                    <FooterLink 
-                        image={footerLinkContact} 
-                        link="/contact"
+                    <FooterLink
+                        image={footerLinkLinkedin}
+                        link="https://www.linkedin.com/in/neary-nc-9bb7a5297/"
                     />
                 </div>
                 <div className="link-item">
-                    <FooterLink 
-                        image={footerLinkInstagram} 
-                        link="https://www.instagram.com/kibishiii_/?hl=fr" 
-                    />
-                </div>
-                <div className="link-item">
-                    <FooterLink 
-                        image={footerLinkLinkedin} 
-                        link="https://www.linkedin.com/in/neary-nc-9bb7a5297/" 
-                    />
-                </div>
-                <div className="link-item">
-                    <FooterLink 
-                        image={footerLinkGithub} 
-                        link="https://github.com/Kibiiiii" 
+                    <FooterLink
+                        image={footerLinkGithub}
+                        link="https://github.com/Kibiiiii"
                     />
                 </div>
             </div>
