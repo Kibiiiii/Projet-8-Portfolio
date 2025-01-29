50e7a953a8905d761b8a7256b56c6e5c25f58843
Neary <kibishiigaming@gmail.com>
2025-01-29 23:56:07 +0100

    Design de la page about me.

--- portfolio/src/pages/AboutMe.jsx
+++ portfolio/src/pages/AboutMe.jsx
@@ -1,18 +1,21 @@
 import React from "react";
 import Datas from "../datas/aboutme.json";
 import Frame from "../assets/images/frame-photo.png";
+import Me from "../assets/images/me.JPG";
 
 const AboutMe = () => {
     const data = Datas[0];
 
     return (
         <div>
-            <h1>{data.title}</h1>
-            <div>
-                <img src={Frame} alt="Cadre" />
-                <h2>{data.id}</h2>
+            <h1 className="about-me-title">{data.title}</h1>
+            <div className="about-me-container">
+                <div className="me-frame">
+                    <img className="me"src={Me} alt="Moi" />
+                    <img className="about-me-frame"src={Frame} alt="Cadre" />
+                </div>
+            <p className="about-me-text">{data.me}</p>
             </div>
-            <p>{data.me}</p>
         </div>
     );
 };
