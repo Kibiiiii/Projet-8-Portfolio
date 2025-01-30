188c1c1a63de3d413725f4db8652c8f9a3c04e03
Neary <kibishiigaming@gmail.com>
2025-01-30 23:33:53 +0100

    Ajout du composant back home sur la page skills.

--- portfolio/src/pages/Skills.jsx
+++ portfolio/src/pages/Skills.jsx
@@ -1,12 +1,14 @@
 import React from "react";
+import BackHome from "../components/Back-Home";
 
 const Skills = () => {
     return (
+        <>
+            <BackHome />
         <div className="skills-page">
-            <h1>Mes Compétences</h1>
-            <p>Bienvenue sur la page des compétences ! Ici, vous trouverez mes principales compétences techniques et personnelles.</p>
-            {/* Ajoute ici le contenu détaillé de tes compétences */}
+
         </div>
+        </>
     );
 };
 
