e148a624415679e14966104445905924a477a87a
Neary <kibishiigaming@gmail.com>
2025-01-30 23:28:49 +0100

    Creation de la modale avec details sur les projets.

--- portfolio/src/pages/Projets.jsx
+++ portfolio/src/pages/Projets.jsx
@@ -1,31 +1,61 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
 import Datas from "../datas/projets.json";
 import BackHome from "../components/Back-Home";
+import "../styles/modale-projets.scss";
+
 
 const GalleryProjets = () => {
+    const [selectedProject, setSelectedProject] = useState(null);
+
+    const openModal = (project) => {
+        setSelectedProject(project);
+    };
+
+    const closeModal = () => {
+        setSelectedProject(null);
+    };
+
     return (
         <>
-        <BackHome />
-        <div>
-        <h1 className="projets-title">Mes Projets</h1>
-        <div className="gallery">
-            {Datas.map((item) => (
-                <div key={item.id} className="gallery-item">
-                    <p className="gallery-title">{item.title}</p>
-                    <Link to={`/projets/:id/${item.id}`}>
-                        <img
-                            src={item.cover}
-                            alt={item.title}
-                            className="gallery-image"
-                            onClick={() => console.log("Image clicked!")}
-                        />
-                </Link>
+            <BackHome />
+            <div>
+                <h1 className="projets-title">Mes Projets</h1>
+                <div className="gallery">
+                    {Datas.map((item) => (
+                        <div
+                            key={item.id}
+                            className="gallery-item"
+                            onClick={() => openModal(item)}
+                        >
+                            <p className="gallery-title">{item.title}</p>
+                            <img
+                                src={item.cover}
+                                alt={item.title}
+                                className="gallery-image"
+                            />
+                        </div>
+                    ))}
                 </div>
-            ))}
-        </div>
-        </div>
-        </>
+
+                {/* Modale */}
+                {selectedProject && (
+                    <div className="modal" onClick={closeModal}>
+                        <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+                            <span className="close" onClick={closeModal}>
+                                &times;
+                            </span>
+                            <h2 className="modal-title">{selectedProject.title}</h2>
+                            <p className="description">{selectedProject.description}</p>
+                            <ul className="objectifs">
+                {selectedProject.objectifs.split(", ").map((objectif, index) => (
+                    <li key={index}>{objectif}</li>
+                ))}
+            </ul>
+                        </div>
+                    </div>
+                )}
+            </div>
+            </>
     );
 };
 
