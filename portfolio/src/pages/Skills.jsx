161d1d7806c3f3c8d47fce8e7b45f8b44ed797c5
Neary <kibishiigaming@gmail.com>
2025-01-31 15:56:14 +0100

    Stylisation de la page skills

--- portfolio/src/pages/Skills.jsx
+++ portfolio/src/pages/Skills.jsx
@@ -1,32 +1,43 @@
 import React from "react";
 import BackHome from "../components/Back-Home";
-import logoHtmlCss from "../assets/images/competences/html-css.jpg";
-import logoSass from "../assets/images/competences/sass.jpg";
-import logoMongoDb from "../assets/images/competences/mongodb.jpg";
-import logoJs from "../assets/images/competences/js.jpg";
-import logoGit from "../assets/images/competences/git.jpg";
-import logoGitHub from "../assets/images/competences/github.jpg";
-import logoNode from "../assets/images/competences/node.jpg";
-import logoExpress from "../assets/images/competences/express.jpg";
-import logoReact from "../assets/images/competences/react.jpg";
-import logoPhotoshop from "../assets/images/competences/photoshop.jpg";
-import logoTrello from "../assets/images/competences/trello.jpg";
-import logoGitHub2 from "../assets/images/competences/github2.jpg";
+import logoHtmlCss from "../assets/images/competences/html-css.png";
+import logoSass from "../assets/images/competences/sass.png";
+import logoMongoDb from "../assets/images/competences/mongodb.png";
+import logoJs from "../assets/images/competences/js.png";
+import logoGit from "../assets/images/competences/git.png";
+import logoGitHub from "../assets/images/competences/github.png";
+import logoNode from "../assets/images/competences/node.png";
+import logoExpress from "../assets/images/competences/express.png";
+import logoReact from "../assets/images/competences/react.png";
+import logoPhotoshop from "../assets/images/competences/photoshop.png";
+import logoTrello from "../assets/images/competences/trello.png";
+import logoGitCat from "../assets/images/competences/gitcat.png";
 
 const logos = [
     logoHtmlCss, logoSass, logoMongoDb, logoJs, logoGit, 
     logoGitHub, logoNode, logoExpress, logoReact, logoPhotoshop, 
-    logoTrello, logoGitHub2
+    logoTrello, logoGitCat
 ];
+
+const repeatedLogos = [...logos, ...logos, ...logos, ...logos, ...logos, ...logos,...logos, ...logos, ...logos,...logos, ...logos, ...logos,...logos, ...logos, ...logos,...logos, ...logos, ...logos,...logos, ...logos, ...logos];
 
 const Skills = () => {
     return (
         <>
             <BackHome />
             <div className="skills">
+            <h1 className="skills-title">Mes Compétences</h1>
                 <div className="skills-track">
-                    {logos.concat(logos).map((logo, index) => ( // Double le contenu pour un scroll fluide
-                        <div className="skills-item" key={index}>
+                    {repeatedLogos.map((logo, index) => (
+                        <div className="skills-item" key={`line1-${index}`}>
+                            <img src={logo} alt="Skill logo" />
+                        </div>
+                    ))}
+                </div>
+
+                <div className="skills-track-reverse">
+                    {repeatedLogos.map((logo, index) => (
+                        <div className="skills-item" key={`line2-${index}`}>
                             <img src={logo} alt="Skill logo" />
                         </div>
                     ))}
